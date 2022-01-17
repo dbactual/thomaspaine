6b5bfc4594c9f17f6c1c51da91044f33d5c0faee
David Berton <db@dbactual.org>
2022-01-17 15:21:08 -0500

    Update CI/CD, moving from make to task.

--- script/gen-timeline.sh
+++ script/gen-timeline.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 mkdir -p gen/
 WRITINGS_INDEX_FILE=./gen/writings_index.md
 WRITINGS_FILE=./gen/writings.md
-WRITINGS_URI=/pages/writings.html
+WRITINGS_URI=/writings.html
 
 TIMELINE_INDEX_FILE=./gen/timeline_index.md
 TIMELINE_FILE=./gen/timeline.md
-TIMELINE_URI=/pages/timeline.html
+TIMELINE_URI=/timeline.html
 
 OLDIFS=$IFS
 
@@ -16,7 +17,7 @@ OLDIFS=$IFS
 
 function get_headers() {
     IFS=$'\n'
-    for a in $(ag --noheading --nobreak --nonumbers "$@" | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
+    for a in $(grep -r "^PubDate: " content | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
         IFS=$OLDIFS b=( $a )
         F=${b[0]}
         F=${F/.md/.html} # fix file extension
@@ -60,6 +61,7 @@ while read -r a; do
     F=${b[0]}
     TITLE=${b[@]:1}
     TITLE=${TITLE//\"/}
+    TITLE=${TITLE//\'/}
     TITLES[$F]=$TITLE
 done  < <(get_headers "^title: " content)
 
@@ -68,6 +70,16 @@ SORTED_YEARS=$(for k in "${!YEARS[@]}"
                    echo $k
                done | sort)
 
+function output_timeline_index()
+{
+    local F=$1
+    echo "<a name=top></a>" >> ${F}
+    # generate the shortcut navigation per year
+    for key in $(echo ${SORTED_YEARS}); do
+        echo "<a href=\"${TIMELINE_URI}#${key}\">${key}</a>" >> ${F}
+    done
+}
+
 cat > ${TIMELINE_INDEX_FILE} <<- EOM
 ---
 title: Timeline Index
@@ -75,11 +87,7 @@ title: Timeline Index
 
 EOM
 
-echo "<a name=top></a>" >> ${TIMELINE_INDEX_FILE}
-# generate the shortcut navigation per year
-for key in $(echo ${SORTED_YEARS}); do
-    echo "<a href=\"${TIMELINE_URI}#${key}\">${key}</a>" >> ${TIMELINE_INDEX_FILE}
-done
+output_timeline_index ${TIMELINE_INDEX_FILE}
 
 cat > ${TIMELINE_FILE} <<- EOM
 ---
@@ -87,7 +95,11 @@ title: Timeline
 ---
 
 <p>
-<!--#include virtual="/pages/timeline_index.html" -->
+EOM
+
+output_timeline_index ${TIMELINE_FILE}
+
+cat >> ${TIMELINE_FILE} <<- EOM
 </p>
 <hr/>
 EOM
@@ -129,6 +141,19 @@ if [ "${UNKNOWN_CATEGORIES}" != "" ]; then
     exit 1
 fi
 
+echo "outputting."
+
+function output_works_index ()
+{
+    local F=$1
+    # generate the shortcut navigation
+    echo "<a name=top></a>" >> ${F}
+    for key in ${CATEGORIES[@]}; do
+        TITLE=$(make_title ${key})
+        echo "<a href=\"${WRITINGS_URI}#${key}\">${TITLE}</a><br/>" >> ${F}
+    done
+}
+
 cat > ${WRITINGS_INDEX_FILE} <<- EOM
 ---
 title: Writings Index
@@ -137,15 +162,7 @@ title: Writings Index
 <p>
 EOM
 
-# generate the shortcut navigation
-echo "<a name=top></a>" >> ${WRITINGS_INDEX_FILE}
-for key in ${CATEGORIES[@]}; do
-    TITLE=$(make_title ${key})
-    echo "<a href=\"${WRITINGS_URI}#${key}\">${TITLE}</a><br/>" >> ${WRITINGS_INDEX_FILE}
-done
-echo "</p>" >> ${WRITINGS_INDEX_FILE}
-
-# ------
+output_works_index ${WRITINGS_INDEX_FILE}
 
 cat > ${WRITINGS_FILE} <<- EOM
 ---
@@ -153,7 +170,11 @@ title: Writings
 ---
 
 <p>
-<!--#include virtual="/pages/writings_index.html" -->
+EOM
+
+output_works_index ${WRITINGS_INDEX_FILE}
+
+cat >> ${WRITINGS_FILE} <<- EOM
 </p>
 <hr />
 <ul>
@@ -162,7 +183,7 @@ EOM
 # generate the writings
 for key in ${CATEGORIES[@]}; do
     TITLE=$(make_title ${key})
-    echo -n "${key} "
+    echo "${key}:"
     echo "" >> ${WRITINGS_FILE}
     echo "<li><a name=\"${key}\"></a><h4>${TITLE}</h4><ul>" >> ${WRITINGS_FILE}
 
@@ -170,14 +191,15 @@ for key in ${CATEGORIES[@]}; do
 
     # list all top-level files with titles
     IFS=$'\n'
-    for a in $(ag --noheading --nobreak --nonumbers '^title: ' content/works/${key} --depth 0 | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
+    for a in $(grep '^title: ' content/works/${key}/*.md | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
         IFS=$OLDIFS b=( $a )
         F=${b[0]}
         F=${F/.md/.html} # fix file extension
         F=${F/content/} # remove path prefix
         TITLE=${b[@]:1}
         TITLE=${TITLE//\"/}
-        echo "[${F}] [${TITLE}]"
+        TITLE=${TITLE//\'/}
+        echo " ${F}: ${TITLE}"
         echo "<li><a href=\"${F}\">${TITLE}</a></li>" >> ${WRITINGS_FILE}
     done
     IFS=$OLDIFS
@@ -185,17 +207,18 @@ for key in ${CATEGORIES[@]}; do
     echo "<!-- BEGIN SECTION -->" >> ${WRITINGS_FILE}
 
     # do each section
-    for s in $(find content/works/${key} -mindepth 1 -maxdepth 1 -type dir | xargs basename | sort); do
+    for s in $(find content/works/${key} -mindepth 1 -maxdepth 1 -type d | sed 's!.*/!!' | sort); do
         echo "<li>$(make_title $s)<ul>" >> ${WRITINGS_FILE}
         IFS=$'\n'
-        for a in $(ag --noheading --nobreak --nonumbers '^title: ' content/works/${key}/${s} | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
+        for a in $(grep '^title: ' content/works/${key}/${s}/*.md | awk 'BEGIN{FS=":"} {print $1 $3}' | sort -k2); do
             IFS=$OLDIFS b=( $a )
             F=${b[0]}
             F=${F/.md/.html} # fix file extension
             F=${F/content/} # remove path prefix
             TITLE=${b[@]:1}
             TITLE=${TITLE//\"/}
-            echo "[${F}] [${TITLE}]"
+            TITLE=${TITLE//\'/}
+            echo " ${F}: ${TITLE}"
             echo "<li><a href=\"${F}\">${TITLE}</a></li>" >> ${WRITINGS_FILE}
         done
         IFS=$OLDIFS
